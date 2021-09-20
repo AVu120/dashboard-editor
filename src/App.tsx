30049533af49b1d416f95c629d77f089dd53b7e2
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-09-21 09:22:55 +1000

    Add support for sass.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import * as React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
+import React, { FC } from "react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import styles from "./App.module.scss";
 
-export const App = () => (
+export const App: FC = () => (
   <ChakraProvider theme={theme}>
-    <ColorModeSwitcher
-      style={{ position: "fixed", top: "20px", right: "20px" }}
-    />
+    <div className={styles.toggleDarkModeButtonContainer}>
+      <ColorModeSwitcher
+      // style={{ position: "fixed", top: "20px", right: "20px" }}
+      />
+    </div>
     <div>App Placeholder</div>
   </ChakraProvider>
 );
