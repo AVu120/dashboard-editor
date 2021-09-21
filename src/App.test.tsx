a38bb45dd0e48041f3160229e2a2916cc012acf1
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-09-21 17:23:59 +1000

    Add basic rendering test.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-// import { screen } from "@testing-library/react"
+import { screen } from "@testing-library/react";
 import { render } from "./test-utils";
 import { App } from "./App";
 
-test("renders learn react link", () => {
+beforeEach(() => {
   render(<App />);
+});
+test("renders app", () => {
+  expect(screen.getByText(/editor mode/i)).toBeInTheDocument();
 });
